afdcfc2e9c1340170ec15f6c1ffc6a15475e8eb0
guizul <lhermesou33@gmail.com>
2023-09-18 10:40:15 -0300

    arrumando coisinhas

--- Vinicius-Ferreira/projeto/src/index.js
+++ Vinicius-Ferreira/projeto/src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.scss';
-import App from './App';
-import {BrowserRouter,Routes, Route} from 'react-router-dom'
+import Login from './pages/login';
+import {BrowserRouter,Routes, Route} from 'react-router-dom';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -10,7 +10,7 @@ root.render(
   <React.StrictMode>
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<App/>}></Route>
+        <Route path="/login" element={<Login/>}></Route>
       </Routes>
     </BrowserRouter>
   </React.StrictMode>
--- Vinicius-Ferreira/projeto/src/pages/login/index.js
+++ Vinicius-Ferreira/projeto/src/pages/login/index.js
@@ -1,10 +1,13 @@
-import './App.scss';
-import {io5IoPawSharp} from "react-icons/io5"
+import './index.scss';
+import {io5IoPawSharp} from "react-icons/io5";
+import Cabecalho from '../../components/cabecalho';
 
 function App() {
   return (
     <div className="Corpo">
-      <div className="Cabecalho"></div>
+      
+      <Cabecalho/>
+      
       <section>
       <div className="Conteudo">
         <div className="patas">
@@ -12,9 +15,11 @@ function App() {
         </div>
             <h1>Faça seu Login</h1>
         <div className="credencial ">
+          <svg xmlns="http://www.w3.org/2000/svg"height="1em" viewBox="0 0 448 512"> <path d="M224 256A128 128 0 1 0 224 0a128 128 0 1 0 0 256zm-45.7 48C79.8 304 0 383.8 0 482.3C0 498.7 13.3 512 29.7 512H418.3c16.4 0 29.7-13.3 29.7-29.7C448 383.8 368.2 304 269.7 304H178.3z"/></svg>
           <input type="text" placeholder="cpf/email"></input>          
         </div>  
         <div className="senha">
+        <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 448 512"><path d="M144 144v48H304V144c0-44.2-35.8-80-80-80s-80 35.8-80 80zM80 192V144C80 64.5 144.5 0 224 0s144 64.5 144 144v48h16c35.3 0 64 28.7 64 64V448c0 35.3-28.7 64-64 64H64c-35.3 0-64-28.7-64-64V256c0-35.3 28.7-64 64-64H80z"/></svg>
         <input type="password" placeholder="senha"></input>          
         </div>
         <div className="suporte">
