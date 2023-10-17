4c2fa5a88d5154c622e291ecc75a5389fb4fa058
susu <vinicius29.vht@gmail.com>
2023-10-17 08:06:32 -0300

    commitei

---
+++ Vinicius-Trajano/projeto/src/pages/Perfil/index.js
@@ -0,0 +1,25 @@
+import './index.scss';
+import Cabecalho from '../../components/cabecalho';
+import Rodape from '../../components/rodape';
+import SectionDecoration from '../../components/section-decoration';
+
+
+export default function Perfil(){
+
+    return (
+
+        <div className='corpo'>
+    
+            <Cabecalho/>
+    
+    
+    
+    
+        </div>
+    
+    
+    
+    
+    )
+}
+
