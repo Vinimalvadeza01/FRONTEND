4968a03a62cd419fc4bd6d2aaedaaf532416b63a
susu <vinicius29.vht@gmail.com>
2023-10-17 09:49:51 -0300

    aqwseraswefrserf

--- Vinicius-Trajano/projeto/src/pages/Perfil/index.js
+++ Vinicius-Trajano/projeto/src/pages/Perfil/index.js
@@ -1,4 +1,5 @@
 import './index.scss';
+import { Link } from 'react-router-dom';
 import Cabecalho from '../../components/cabecalho';
 import Rodape from '../../components/rodape';
 import SectionDecoration from '../../components/section-decoration';
@@ -11,9 +12,33 @@ export default function Perfil(){
         <div className='corpo'>
     
             <Cabecalho/>
+
+            <div className='center'>
+            <SectionDecoration/>
+
+            <div className='menu-alt'>
+
+                    <div className='menu'>
+                        <Link className='link-p' >Informações de usuário</Link>
+                        <Link className='link' >Informações de endereço</Link>
+                        <Link className='link' >Favoritos</Link>
+                        <Link className='link' >Meus edidos</Link>
+                        <Link className='link' >Trocar de conta</Link>
+                        <Link className='link' >Sair</Link>
+                    </div>
+                    <hr/>
+                    <div className='alterar'>
+                        <h5>Deseja alterar alguma informação ?</h5>
+                        <button>ALTERAR</button>
+                    </div>
+
+            </div>
+
+
+            </div>
     
     
-    
+            <Rodape/>
     
         </div>
     
