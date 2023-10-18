afcaed39234385acb2216ce1a8965d29c16152ff
Susu <viniciu29.vht@gmail.com>
2023-10-18 09:40:50 -0300

    foi trava

---
+++ Vinicius-Trajano/projeto/src/pages/perfil/informacoes-de-endereco/index.js
@@ -0,0 +1,24 @@
+import './index.scss';
+import { Link } from 'react-router-dom';
+import Cabecalho from '../../../components/cabecalho';
+import Rodape from '../../../components/rodape';
+import SectionDecoration from '../../../components/section-decoration'
+import InputMask from 'react-input-mask';
+
+export default function Endereco(){
+
+    return(
+
+        <div className='corpo'>
+
+         <Cabecalho/>
+
+
+        </div>
+
+    )
+
+
+
+
+}
