f683d0e95e83db01a3b76326e3ee7728e1356db7
guizult <lhermesou33@gmail.com>
2023-10-20 07:59:38 -0300

    subit

---
+++ Guilherme-Almeida/projeto/src/pages/carrinho/index.js
@@ -0,0 +1,14 @@
+import axios from 'axios'
+import './index.scss';
+import Cabecalho from "../../components/cabecalho"
+import {useNavigate} from 'react-router-dom'
+import { useState } from 'react';
+
+return(
+    <div className='pagina-carrinho'>
+
+        <Cabecalho/>
+            
+    </div>
+
+)
