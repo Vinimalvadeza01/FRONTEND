155cb7b85d360436b91a30c51865f3673f19cd1b
guizult <lhermesou33@gmail.com>
2023-10-30 11:55:29 -0300

    subii

--- Guilherme-Almeida/projeto/src/index.js
+++ Guilherme-Almeida/projeto/src/index.js
@@ -4,7 +4,7 @@ import './index.scss';
 import LoginAdm from './pages/loginAdm';
 import {BrowserRouter,Routes, Route} from 'react-router-dom';
 import Home from './pages/home';
-
+import CadastrarProduto from './pages/cadastroProduto';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -13,6 +13,7 @@ root.render(
     <Routes>
       <Route path="/adm/login" element={<LoginAdm/>}></Route>
       <Route path='/' element={<Home/>}></Route>
+      <Route path='/adm/cadastrar-produto' element={<CadastrarProduto/>}></Route>
     </Routes>
   </BrowserRouter>
 </React.StrictMode>
