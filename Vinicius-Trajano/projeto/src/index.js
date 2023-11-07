41ea3cd353aede252a3632370931acd185a94cac
susu <vinicius29.vht@gmail.com>
2023-11-07 10:59:18 -0300

    pagina favoritos em desenvolvimento

--- Vinicius-Trajano/projeto/src/index.js
+++ Vinicius-Trajano/projeto/src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.scss';
 import Perfil from './pages/perfil/informacoes-de-usuario';
 import Endereco from './pages/perfil/informacoes-de-endereco'
+import Favoritos from './pages/perfil/favoritos';
 import {BrowserRouter, Routes, Route} from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -13,6 +14,7 @@ root.render(
       <Routes>
         <Route path='/perfil' exact={true} element={<Perfil/>} ></Route>
         <Route path='/perfil/informacoes-de-endereco' element={<Endereco/>} ></Route> 
+        <Route path='/perfil/favoritos' element={<Favoritos/>} > </Route>
       </Routes>
     </BrowserRouter>
     
