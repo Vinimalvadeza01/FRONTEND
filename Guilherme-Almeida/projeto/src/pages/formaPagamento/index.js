28586b7ca1b8e41027c5975eabee4e3ce6cf20d2
guizult <lhermesou33@gmail.com>
2023-11-16 10:01:37 -0200

    subuirr

---
+++ Guilherme-Almeida/projeto/src/pages/formaPagamento/index.js
@@ -0,0 +1,19 @@
+import './index.js'
+
+import Cabecalho from '../../components/cabecalho-adm';
+import SectionDecoration from '../../../components/section-decoration'
+import axios from 'axios';
+
+import { useState,useEffect } from 'react';
+
+
+function Pagamento(){
+    return(
+        <div className='corpo'>
+            <Cabecalho/>
+                <div className='conteudo'>
+                    vagina, bunda, puta, xereca
+                </div>
+        </div>
+    )
+}
