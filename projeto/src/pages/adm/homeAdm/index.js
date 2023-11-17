3b0f568b7bddaf7351426fa37eb68662445c27b1
Macgithub13 <macgames444@gmail.com>
2023-11-17 04:46:57 -0300

    subindo alterações

--- projeto/src/pages/adm/homeAdm/index.js
+++ projeto/src/pages/adm/homeAdm/index.js
@@ -1,5 +1,8 @@
 import './index.scss';
 import CabecalhoAdm from '../../../components/cabecalho-adm';
+import SectionDecoration from '../../../components/section-decoration';
+import{Link} from 'react-router-dom';
+import storage from 'local-storage';
 
 export default function HomeAdm(){
 
@@ -8,6 +11,25 @@ export default function HomeAdm(){
         <div className='page-home-adm'>
 
             <CabecalhoAdm/>
+
+            <div className='div-selecionar'>
+
+                <SectionDecoration/>
+
+                <h1>Bem vindo {storage('adm-logado').data.Adm}</h1>
+
+                <h2>O que deseja fazer ?</h2>
+
+                <div className='div-buttons'>
+                    <Link className='Link redirecionar-home-adm' to='/adm/cadastrar-produto'>Cadastrar Produto</Link>
+                    <Link className='Link redirecionar-home-adm' to='/adm/consulta/produtos'>Consultar Produtos</Link>
+                    <Link className='Link redirecionar-home-adm' to='/adm/consulta/clientes'>Consultar Clientes</Link>
+                </div>
+            </div>
+
+            <div className='infs-totais'></div>
+
+            <div className='grafico'></div>
         </div>
     );
 }
