cd19924a12a3c6bb5f1142d5e041f3f0ead37f1e
guizult <lhermesou33@gmail.com>
2023-11-22 10:48:38 -0200

    subir

--- projeto/src/pages/produto-cliente/index.js
+++ projeto/src/pages/produto-cliente/index.js
@@ -25,23 +25,24 @@ const diminuirContador = () => {
             <div className='conteudo'>
                 
                 <div className='imgs'>
-                    <div className='quadrado'>
+                    <div className='quadrado-produto'>
 
                     </div>
-                    <div className='quadrado'>
+                    <div className='quadrado-produto'>
                         
                     </div>
-                    <div className='quadrado'>
+                    <div className='quadrado-produto'>
                         
                     </div>
-                    <div className='quadrado'>
+                    <div className='quadrado-produto'>
                         
                     </div>
 
                 </div>
-                <img url='https://www.figma.com/file/EHFu5VH2i4ta2X8k5G3U4Y/Prot%C3%B3tipo-P%C3%A1ginas?type=design&node-id=1237-60&mode=design&t=gFJW6LGp7BO1uSAB-4' />
-
-                <div className='nome-pedido'>
+                <div className='imagem-produto-cliente'>
+                <img src='/assets/images/produto7.png' />
+                </div>
+                <div className='qw'>
                     <label>Semente de Graminha para Gatos Zoe Pet Azevém, Aveia e Cevada 130g</label>
                     <svg width="15" height="15" viewBox="0 0 71 68" fill="none" xmlns="http://www.w3.org/2000/svg">
                     <path d="M39.2753 2.49387C38.5724 1.04265 37.0872 0.119141 35.4561 0.119141C33.8249 0.119141 32.3529 1.04265 31.6368 2.49387L23.1098 19.9481L4.06669 22.745C2.47534 22.9825 1.14922 24.0907 0.658552 25.6079C0.167886 27.125 0.565723 28.8006 1.70619 29.9219L15.5244 43.5238L12.2621 62.7459C11.9969 64.3291 12.66 65.9386 13.9728 66.8753C15.2857 67.812 17.0229 67.9308 18.4551 67.1788L35.4693 58.1416L52.4835 67.1788C53.9157 67.9308 55.6529 67.8252 56.9658 66.8753C58.2786 65.9254 58.9417 64.3291 58.6765 62.7459L55.401 43.5238L69.2192 29.9219C70.3596 28.8006 70.7707 27.125 70.2668 25.6079C69.7629 24.0907 68.45 22.9825 66.8587 22.745L47.8023 19.9481L39.2753 2.49387Z" fill="#FEC808"/>
